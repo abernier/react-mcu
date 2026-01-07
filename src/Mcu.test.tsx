e19f9bfa36aa9c9f313df4f2f673c29a6e0491c5
Antoine BERNIER <antoine.bernier@gmail.com>
2026-01-07 13:19:23 +0100

    fix test

--- src/Mcu.test.tsx
+++ src/Mcu.test.tsx
@@ -1,11 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
 import { Mcu } from './Mcu.js';
 
 describe('Mcu', () => {
   it('should render hello world div', () => {
-    render(<Mcu />);
-    const divElement = screen.getByText('Hello World');
+    const { getByText } = render(<Mcu />);
+    const divElement = getByText('Hello World');
     expect(divElement).toBeTruthy();
   });
 });
