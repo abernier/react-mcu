c61c3b1891917d23e30fbb403c736c3022d17a79
Antoine BERNIER <antoine.bernier@gmail.com>
2026-02-06 18:01:46 +0100

    .

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,8 +4,5 @@ const config: StorybookConfig = {
   framework: "@storybook/react-vite",
   stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: ["@storybook/addon-docs", "@storybook/addon-themes"],
-  docs: {
-    autodocs: true,
-  },
 };
 export default config;
