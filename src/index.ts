5bd0893789139215677afce8f1d71ea0d560ded2
Antoine BERNIER <antoine.bernier@gmail.com>
2026-02-18 08:53:14 +0100

    public api exports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
-export {
-  DEFAULT_ADAPTIVE_SHADES,
-  DEFAULT_BLEND,
-  DEFAULT_CONTRAST,
-  DEFAULT_CONTRAST_ALL_COLORS,
-  DEFAULT_SCHEME,
-  builder,
-  schemeNames,
-} from "./lib/builder";
+export { builder } from "./lib/builder";
 export { Mcu } from "./Mcu";
 export { useMcu } from "./Mcu.context";
